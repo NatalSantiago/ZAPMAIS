f041088cfc51a254507a2f51c3fbb13c8ba3120b
NatalSantiago <natal.santiago.filha@gmail.com>
2024-02-28 09:58:56 -0300

    Backup dia 28-02-2024 sistema ZapMais usando o ampps,appsmit e grok funcionando tudo ok

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Autor: Thiago Ciavolela
+Data: 18/10/2023
+Descrição: Projeto OpenSource trabalhado emcima da EvolutionApi, para disparos em Massa.
+*/
+
+
+// Configurações EvolutionApi
+
+//KEY DO ARQUIVO ENV
+$apiKey = "B6D711FCDE4D4FD5936544120E713976";
+
+//Url envio de Texto
+$url_api_txt = "http://10.0.1.106:8080/message/sendText/envio01";
+
+//Url envio com imagem
+$url_api_image = "http://10.0.1.106:8080/message/sendMedia/envio01";
+
+//Delay entre disparo de mensagem, tempo em segundos
+$delay = "25";
